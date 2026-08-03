079a1f4b94fb888e0d7d7ff10399b052c26c62a8
Adarsh Kumar <adarshku123456789@gmail.com>
2026-08-03 18:05:20 +0530

    feat(ui): integrate presentation layer components

--- DigitalClock/Src/main.cpp
+++ DigitalClock/Src/main.cpp
@@ -18,8 +18,7 @@ int main()
 
         display.render(clock, date);
 
-        std::this_thread::sleep_for(
-            std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     return 0;
